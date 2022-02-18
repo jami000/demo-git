6b6d9cb731472533e144b9b7c7d6e2d00ca45083
Minh Nhat <88365454+jami000@users.noreply.github.com>
2022-02-18 11:26:36 +0700

    Remote update

--- demogit_test/lib/main.dart
+++ demogit_test/lib/main.dart
@@ -46,7 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
               'master code update Text', style: TextStyle(fontSize: 30),
             ),
             Text(
-              'dev-nhat code update Text 2',
+              'Remote code',
               style: TextStyle(fontSize: 30),
             ),
           ],
